bb7fb8ee5bbd7d84cc3e9d279bfcee68225ce30a
Ryota Takeuchi <mathmyg@protonmail.com>
2025-06-24 19:32:41 +0900

    Initial Commit

---
+++ src/main/java/io/github/ryang_ryota/oogiri/OogiriApplication.java
@@ -0,0 +1,13 @@
+package io.github.ryang_ryota.oogiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OogiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OogiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ryang_ryota/oogiri/OogiriApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ryang_ryota.oogiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OogiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
